4383c927ec06f132b56f79c794902798c3dd628b
neerajguptaa2 <neerajguptaa@ymail.com>
2016-11-24 03:29:46 +0530

    Assignment

---
+++ src/Assignment_1_4.java
@@ -0,0 +1,17 @@
+
+public class Assignment_1_4 {
+
+	public static void main(String[] args) {
+		int a=65;
+		char b;
+		
+		int i;
+		
+		for(i=a; i<=90 ; i++ )
+		{
+			b=(char)i;
+			System.out.println(i + "-" + b);
+		}
+	}
+
+}
